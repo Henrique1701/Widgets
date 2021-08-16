8236096ae7698e0afce1435860e4d03fe83bd855
José Henrique Fernandes Silva <jhfs@cin.ufpe.br>
2021-08-16 16:52:57 -0300

    Adiciona projeto base WidgetTest

---
+++ WidgetTest/WidgetTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WidgetTest
+//
+//  Created by José Henrique Fernandes Silva on 16/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WidgetTest/WidgetTest/WidgetTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetTestApp.swift
+//  WidgetTest
+//
+//  Created by José Henrique Fernandes Silva on 16/08/21.
+//
+
+import SwiftUI
+
+@main
+struct WidgetTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
