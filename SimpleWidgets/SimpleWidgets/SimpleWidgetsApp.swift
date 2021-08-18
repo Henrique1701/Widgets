fb36a44f6669da887f674367ea1663e030eac205
José Henrique Fernandes Silva <jhfs@cin.ufpe.br>
2021-08-18 12:27:34 -0300

    Adiciona projeto base SimpleWidgets

---
+++ SimpleWidgets/SimpleWidgets/SimpleWidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWidgetsApp.swift
+//  SimpleWidgets
+//
+//  Created by José Henrique Fernandes Silva on 18/08/21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleWidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
