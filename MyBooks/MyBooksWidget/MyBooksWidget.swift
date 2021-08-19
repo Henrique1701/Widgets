f88c50c7c18666120c29edcdf1ff63b500a615d9
José Henrique Fernandes Silva <jhfs@cin.ufpe.br>
2021-08-19 11:09:51 -0300

    Adiciona InformationWidget

--- MyBooks/MyBooksWidget/MyBooksWidget.swift
+++ MyBooks/MyBooksWidget/MyBooksWidget.swift
@@ -30,10 +30,44 @@ struct ImageProvider: TimelineProvider {
     }
 }
 
+struct InformationProvider: TimelineProvider {
+    func placeholder(in context: Context) -> InformationEntry {
+        let title = "Dom Casmurro"
+        let author = "Machado de Assis"
+
+        return InformationEntry(date: Date(), title: title, author: author)
+    }
+    
+    func getSnapshot(in context: Context, completion: @escaping (InformationEntry) -> Void) {
+        let title = "Dom Casmurro"
+        let author = "Machado de Assis"
+        let entry = InformationEntry(date: Date(), title: title, author: author)
+        completion(entry)
+    }
+    
+    func getTimeline(in context: Context, completion: @escaping (Timeline<InformationEntry>) -> Void) {
+        var entries: [InformationEntry] = []
+        
+        let title = "Dom Casmurro"
+        let author = "Machado de Assis"
+        let entry = InformationEntry(date: Date(), title: title, author: author)
+        entries.append(entry)
+        
+        let timeline = Timeline(entries: entries, policy: .never)
+        completion(timeline)
+    }
+}
+
 // MARK: TimelineEntry
 struct ImageEntry: TimelineEntry {
     let date: Date
     let imageName: String
+}
+
+struct InformationEntry: TimelineEntry {
+    let date: Date
+    let title: String
+    let author: String
 }
 
 // MARK: View
@@ -54,6 +88,21 @@ struct ImageWidgetEntryView : View {
     }
 }
 
+struct InformationWidgetEntryView : View {
+    var entry: InformationProvider.Entry
+    
+    var body: some View {
+        VStack {
+            Text("Dom Casmurro")
+                .font(.title3)
+                .fontWeight(.bold)
+            Text("Machado de Assis")
+                .font(.callout)
+                .fontWeight(.light)
+        }
+    }
+}
+
 // MARK: Widget
 struct ImageWidget: Widget {
     let kind: String = "ImageWidget"
@@ -68,18 +117,35 @@ struct ImageWidget: Widget {
     }
 }
 
+struct InformationWidget: Widget {
+    let kind: String = "InformationWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: InformationProvider()) { entry in
+            InformationWidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("Information Widget")
+        .description("This is a widget that shows the information of the last book you read.")
+        .supportedFamilies([.systemSmall])
+    }
+}
+
 // MARK: WidgetBunble
 @main
 struct WidgetContainer: WidgetBundle {
     @WidgetBundleBuilder
     var body: some Widget {
         ImageWidget()
+        InformationWidget()
     }
 }
 
 struct MyBooksWidget_Previews: PreviewProvider {
     static var previews: some View {
         ImageWidgetEntryView(entry: ImageEntry(date: Date(), imageName: "dom_casmurro"))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+        
+        InformationWidgetEntryView(entry: InformationEntry(date: Date(), title: "Dom Casmurro", author: "Machado de Assis"))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
