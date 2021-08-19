6e98f7798ebadd985b15624a4b52abef98e208f4
José Henrique Fernandes Silva <jhfs@cin.ufpe.br>
2021-08-19 09:14:38 -0300

    Adiciona projeto base MyBooks

---
+++ MyBooks/MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by José Henrique Fernandes Silva on 19/08/21.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
